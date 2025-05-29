15eaadbdb134213f74fc11633618cb098e35bacb
Omar Crosby <omar.crosby@gmail.com>
2025-05-28 23:00:16 -0400

    fix(alluredir): avoid duplicate registration of allure directory

--- allure-pytest/src/plugin.py
+++ allure-pytest/src/plugin.py
@@ -17,12 +17,15 @@ from allure_pytest.utils import ALLURE_LABEL_MARK, ALLURE_LINK_MARK
 
 
 def pytest_addoption(parser):
-    parser.getgroup("reporting").addoption('--alluredir',
-                                           action="store",
-                                           dest="allure_report_dir",
-                                           metavar="DIR",
-                                           default=None,
-                                           help="Generate Allure report in the specified directory (may not exist)")
+    group = parser.getgroup("reporting")
+    # Only register --alluredir if it hasn't already been registered
+    if '--alluredir' not in parser._option_string_actions:
+        group.addoption('--alluredir',
+                        action="store",
+                        dest="allure_report_dir",
+                        metavar="DIR",
+                        default=None,
+                        help="Generate Allure report in the specified directory (may not exist)")
 
     parser.getgroup("reporting").addoption('--clean-alluredir',
                                            action="store_true",
